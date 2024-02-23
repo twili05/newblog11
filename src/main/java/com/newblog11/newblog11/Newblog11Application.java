6d5b601410fbb2e69ba7a5b96dc60e27579d0967
Moumita <moumitaKanrar526@gmail.com>
2024-02-23 16:22:07 +0530

    23: new blog

---
+++ src/main/java/com/newblog11/newblog11/Newblog11Application.java
@@ -0,0 +1,13 @@
+package com.newblog11.newblog11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Newblog11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Newblog11Application.class, args);
+	}
+
+}
